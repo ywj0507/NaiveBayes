e062a6ad3c1c90d1c71265abe932a962d11a9a2e
ywj_lab <652376360@bupt.edu.cn>
2018-11-19 20:37:58 +0800

    first commit

---
+++ src/test/demo.java
@@ -0,0 +1,70 @@
+package test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class demo {
+	private int count=0;
+	private int reCursion(int a) {
+		if(a<1 && count != 0) {
+			return count; 
+		}
+		if(count==0) {
+			return -1;
+		}
+		while(a>=1) {
+			count = count * a;
+			a--;
+		}
+		return count; 
+	}
+	//TODO
+	public String longestPalindrome(String s) {
+        char[] c = new char[s.length()];
+//		String str="";
+//		c=str.charAt(0);
+        
+        return "";
+    }
+	/*
+	 * 定义训练集和测试集的元素个数
+	 */
+	public static final int Train_Num = 5000;
+	public static final int Test_Num = 5000;
+	public static final String FileName = "E:\test.txt";
+	
+	/*
+	 *朴素贝叶斯算法实现
+	 *TODO 
+	 */
+	public void NaiveBayesClassif() {
+		HashMap<String,Integer> element = new HashMap<String ,Integer>();
+//		int [][]nation  = new int[Train_Num][Train_Num];
+		String line;
+		try {
+			FileReader read = new FileReader(FileName);
+			
+			while( read.read() !=-1) {
+				
+			}
+			BufferedReader in = new BufferedReader(new FileReader(FileName));
+			line = in.readLine();
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+	}
+}
