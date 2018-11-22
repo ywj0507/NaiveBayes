0a38dd0b7b0a68a2e5a2664d98b8d5b0fa68c1e7
ywj_lab <652376360@bupt.edu.cn>
2018-11-22 21:58:42 +0800

    bayes

--- src/test/demo.java
+++ src/test/demo.java
@@ -1,15 +1,14 @@
 package test;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.HashMap;
 import java.util.Scanner;
 import java.util.Vector;
 
-
+/**
+ * 练手朴素贝叶斯例子
+ * 训练样本 （R,1,3,4,3）
+ */
 public class demo {
 
 	/*
@@ -20,7 +19,10 @@ public class demo {
 	public static final String FileName = "E:/test.txt";
 	//存放读入数据
 	public static Vector<String> inData = new Vector<>();
-	/*
+	private static Vector<int[]> catagory_R = new Vector<>();
+	private static Vector<int[]> catagory_L = new Vector<>();
+	private static Vector<int[]> catagory_B = new Vector<>();
+	/**
 	 * 读取原始数据操作
 	 */
 	public static boolean readData(String url) {
@@ -34,53 +36,92 @@ public class demo {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			return false;
-		}
+		}  
+//		try {
+//			FileReader read = new FileReader(FileName);
+//			int c = 0;
+//			while( read.read() !=-1) {
+//				 System.out.print((char)c);
+//				 c=read.read();
+//			}
+//			BufferedReader in = new BufferedReader(new FileReader(FileName));
+//			line = in.readLine();
+//			while(line!=null) {
+//				System.out.println(line);
+//				line = in.readLine();
+//			}
+//			
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 	}
 	
-	/*
+	/**
 	 * 数据预处理
 	 * TODO
 	 */
-	public static boolean preTreatment(Vector<String> sData) {
+	public static void preTreatment(Vector<String> inData) {
 		//换成每个向量
-		
-		return false;
+		int i=0;
+		while(i<inData.size()) {
+			int[] tem = new int[] {0,0,0,0};
+			String[] sourceStr =  inData.get(i).split("," , 5);
+			switch(sourceStr[0]) {
+				case "R":{
+					for(int j=1;j<5;j++) {
+						tem[j-1] = Integer.parseInt(sourceStr[j]);
+					}
+					catagory_R.add(tem);
+					break;
+				}
+				case "L":{
+					for(int j=1;j<5;j++) {
+						tem[j-1] = Integer.parseInt(sourceStr[j]);
+					}
+					catagory_L.add(tem);
+					break;
+				}
+				case "B":{
+					for(int j=1;j<5;j++) {
+						tem[j-1] = Integer.parseInt(sourceStr[j]);
+					}
+					catagory_B.add(tem);
+					break;
+				}
+			}
+			i++;
+		}
 	}
 	
-	/*
+	/**
 	 *朴素贝叶斯算法实现
 	 *TODO 
 	 */
-	public static void NaiveBayesClassif() {
-		HashMap<String,Integer> element = new HashMap<String ,Integer>();
-//		int [][]nation  = new int[Train_Num][Train_Num];
-		String line;
-		try {
-			FileReader read = new FileReader(FileName);
-			int c = 0;
-			while( read.read() !=-1) {
-				 System.out.print((char)c);
-				 c=read.read();
-			}
-			BufferedReader in = new BufferedReader(new FileReader(FileName));
-			line = in.readLine();
-			while(line!=null) {
-				System.out.println(line);
-				line = in.readLine();
-			}
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	public static double NaiveBayes(int[] x, Vector<int[]> catagory) {
+		if(catagory.size()<=0) {
+			return -1;
 		}
-		
+		double[] ai_y = new double[4];
+		int[] sum_ai = new int[] {0,0,0,0};
+		for(int i = 0; i < 4; i++) {  //计算总数
+			for(int j=0;j<catagory.size();j++) {
+				if(x[i] == catagory.elementAt(j)[i]) {
+					sum_ai[i]++;
+				}
+			}
+		}
+		for(int i=0; i<4;i++) {
+			ai_y[i] = (double)sum_ai[i] / (double)catagory.size(); 
+		}
+		return ai_y[0]*ai_y[1]*ai_y[2]*ai_y[3];
 	}
 	
 	
-	
-	
-	
-	public static void main(String[] args) {
-		NaiveBayesClassif();
+	public  void main(String[] args) {
+		readData("E:/test.txt");
+		
+		
+		
 	}
 }
